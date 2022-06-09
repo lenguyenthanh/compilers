d573eae9d3515fb5b09fe3bc679bef99fa8064de
Thanh Le <lenguyenthanh@hotmail.com>
2022-06-09 23:09:30 +0200

    Parser seems to work

--- Ulc.scala
+++ Ulc.scala
@@ -2,9 +2,18 @@
 //> using lib "org.typelevel::cats-core:2.7.0"
 //> using lib "org.typelevel::cats-parse:0.3.7"
 
-// case class Location(val line: Int, val col: Int, val offset: Int)
-// case class Info(start: Location, end: Location)
-type Info = Unit
+case class Location(val line: Int, val col: Int, val offset: Int)
+case class Info(start: Location, end: Location):
+  def merge(other: Info): Info =
+    this.copy(end = other.end)
+
+enum Token(val lexeme: String, val info: Info):
+  case LeftParen(override val info: Info)  extends Token("(", info)
+  case RightParen(override val info: Info) extends Token(")", info)
+  case Lambda(override val info: Info)     extends Token("\\", info)
+  // case Assign(override val info: Info) extends Token("=", info)
+  case Dot(override val info: Info)                                     extends Token(".", info)
+  case Identifier(override val lexeme: String, override val info: Info) extends Token(lexeme, info)
 
 type NameBinding = Unit
 type Context     = List[(String, NameBinding)]
@@ -20,7 +29,154 @@ enum Term(val info: Info):
       case TMAbs(_, t)            => s"λ.$t"
       case TMApp(_, t1, t2)       => s"($t1 $t2)"
 
-object Term:
+object Lexer:
+  import cats.parse.{ Caret, LocationMap, Numbers as N, Parser as P, Parser0 as P0, Rfc5234 as R }
+  import Token.*
+  val endOfLine: P[Unit]    = R.cr | R.lf
+  val whitespaces: P0[Unit] = P.until0(!R.wsp).void
+  val location              = P.caret.map(c => Location(c.line, c.col, c.offset))
+
+  // token
+  val leftParen  = P.char('(').info.map(p => LeftParen(p._2))
+  val rightParen = P.char(')').info.map(p => RightParen(p._2))
+  // val assign = P.char('=').info.map(p => Assign(p._2))
+  val lambda = P.char('\\').info.map(p => Lambda(p._2))
+  val dot    = P.char('.').info.map(p => Dot(p._2))
+
+  val allow = R.alpha | N.digit | P.charIn('!', '@', '#', '$', '%', '^', '&', '*', '_', '?', '<', '>')
+
+  val identifer = allow.rep.string.info.map(p => Identifier(p._1, p._2))
+
+  val token = (leftParen | rightParen | lambda | dot | identifer).surroundedBy(whitespaces) // | assign
+
+  val parser = token.rep.map(_.toList)
+
+  def scan(str: String): Either[String, List[Token]] =
+    parser.parse(str) match
+      case Right("", ls) => Right(ls)
+      case Right(rest, ls) =>
+        val idx = str.indexOf(rest)
+        Left(s"Parital string $rest")
+      case Left(err) =>
+        val idx = err.failedAtOffset
+        val lm  = LocationMap(str)
+        Left(s"Lexer failed at $idx: $err")
+
+  extension [T](p: P[T])
+    def info: P[(T, Info)] = (location.with1 ~ p ~ location).map { case ((s, t), e) => (t, Info(s, e)) }
+
+trait Parser[A, +B]:
+  def parse(input: List[A]): Either[String, (List[A], B)]
+
+  def |[B1 >: B](that: Parser[A, B1]): Parser[A, B1] = new Parser[A, B1] {
+    def parse(input: List[A]) =
+      Parser.this.parse(input) match
+        case Left(_)         => that.parse(input)
+        case r @ Right(_, _) => r
+  }
+
+object Parser:
+
+  import cats.Functor
+  import cats.Monad
+
+  given [A]: Functor[[x] =>> Parser[A, x]] with
+    def map[B, C](p: Parser[A, B])(f: B => C): Parser[A, C] = new Parser[A, C] {
+      def parse(input: List[A]) =
+        p.parse(input).map { case (rest, a) => (rest, f(a)) }
+    }
+
+  given [A]: Monad[[x] =>> Parser[A, x]] = new Monad[[x] =>> Parser[A, x]] {
+
+    def pure[B](b: B): Parser[A, B] = new Parser[A, B] {
+      def parse(input: List[A]) = Right(input, b)
+    }
+
+    def flatMap[B, C](p: Parser[A, B])(f: B => Parser[A, C]): Parser[A, C] = new Parser[A, C] {
+      def parse(input: List[A]) =
+        p.parse(input).flatMap((rest, b) => f(b).parse(rest))
+    }
+
+    // @tailrec is this a hack
+    def tailRecM[B, C](b: B)(f: B => Parser[A, Either[B, C]]): Parser[A, C] = new Parser[A, C] {
+      def parse(input: List[A]) =
+        f(b).parse(input) match
+          case Left(err)               => Left(err)
+          case Right((rest, Right(c))) => Right(rest, c)
+          case Right((rest, Left(b1))) => tailRecM(b1)(f).parse(rest)
+    }
+  }
+
+object Parser1:
+  import scala.reflect.Typeable
+
+  import Token.*
+  import Parser.*
+  import Parser.given
+  import cats.instances.all.*
+  import cats.syntax.all.*
+
+  enum Term(val info: Info):
+    case TMVar(override val info: Info, val name: String)               extends Term(info)
+    case TMAbs(override val info: Info, val arg: TMVar, val term: Term) extends Term(info)
+    case TMApp(override val info: Info, val t1: Term, val t2: Term)     extends Term(info)
+
+  def test[T: Typeable](token: Token): Boolean =
+    token match
+      case _: T => true
+      case _    => false
+
+  def token[T: Typeable]: Parser[Token, T] = new Parser[Token, T] {
+    def parse(input: List[Token]): Either[String, (List[Token], T)] =
+      println(s"token $input")
+      input match
+        case head :: tail =>
+          head match
+            case t: T => Right(tail, t)
+            case _    => Left("unexpected token")
+        case _ => Left("Empty input")
+  }
+
+  import Term.*
+  def term: Parser[Token, Term]           = lambda | group | variable | app
+  def termWithoutApp: Parser[Token, Term] = lambda | group | variable
+  lazy val variable: Parser[Token, TMVar] =
+    println("variable")
+    token[Identifier].map(id => TMVar(id.info, id.lexeme))
+
+  lazy val lambda: Parser[Token, Term] =
+    println("lambda")
+    for
+      l <- token[Lambda]
+      _ = println(l)
+      id <- variable
+      _ = println(id)
+      _ <- token[Dot]
+      t <- term
+      _ = println(t)
+    yield TMAbs(l.info.merge(t.info), id, t)
+
+  lazy val app =
+    println("app")
+    for
+      t1 <- termWithoutApp
+      t2 <- termWithoutApp
+    yield TMApp(t1.info.merge(t2.info), t1, t2)
+
+  lazy val group =
+    for
+      _ <- token[LeftParen]
+      t <- term
+      _ <- token[RightParen]
+    yield t
+
+  def parse(tokens: List[Token]): Either[String, Term] =
+    term.parse(tokens) match
+      case Left(err)          => Left(err)
+      case Right((Nil, term)) => Right(term)
+      case _                  => Left("Partial Parse")
+
+object Evaluation:
   import Term.*
 
   def termShift(d: Int): Term => Term = termShift(d, 0)
@@ -35,8 +191,7 @@ object Term:
   // The substitution [j ↦ s] t of term s for numbered j in term t
   def termSubst(j: Int, s: Term): Term => Term =
     def onVar(v: TMVar, c: Int): Term =
-      if v.index == j + c then
-        termShift(c)(s)
+      if v.index == j + c then termShift(c)(s)
       else v
     map(onVar, 0)
 
@@ -54,9 +209,6 @@ object Term:
   def isVal: Term => Boolean =
     case TMAbs(_, _) => true
     case _           => false
-
-object Evaluation:
-  import Term.*
 
   def eval1(term: Term): (Term, Boolean) =
     term match
@@ -78,11 +230,19 @@ object Evaluation:
 def main =
   import Term.*
   import Evaluation.*
-  // (λ.1 0 2) (λ.0) -> 0 (λ.0) 1
-  val t = TMApp(
-    (),
-    TMAbs((), TMApp((), TMApp((), TMVar((), 1, 3), TMVar((), 0, 3)), TMVar((), 2, 3))),
-    TMAbs((), TMVar((), 0, 1))
-  )
+  val x = "\\x. \\y. y"
+  // val x  = "\\x. x"
+  // val x = "x \\x."
+  val t = for
+    ts <- Lexer.scan(x)
+    t  <- Parser1.parse(ts)
+  yield t
   println(t)
-  println(eval(t))
+  // (λ.1 0 2) (λ.0) -> 0 (λ.0) 1
+  // val t = TMApp(
+  //   (),
+  //   TMAbs((), TMApp((), TMApp((), TMVar((), 1, 3), TMVar((), 0, 3)), TMVar((), 2, 3))),
+  //   TMAbs((), TMVar((), 0, 1))
+  // )
+  // println(t)
+  // println(eval(t))
